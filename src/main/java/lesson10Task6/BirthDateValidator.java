c987a2fdf5bbba2cd28b1a0fb20b8f6413023c57
TeresaTao127 <taoleiluck2020@gmail.com>
2023-09-13 11:52:16 +0100

    lesson10 Task6 update functionality separately

--- src/main/java/lesson10Task6/BirthDateValidator.java
+++ src/main/java/lesson10Task6/BirthDateValidator.java
@@ -10,27 +10,26 @@ import java.util.regex.Pattern;
 public class BirthDateValidator {
     public static void main(String[] args) {
         String[] birthDates = {
-                "1997-08-03",
-                "2024-07-27",
-                "2021-08"
+            "1997-08-03",
+            "2024-07-27",
+            "2021-08"
         };
 
         for (String birthDateString : birthDates) {
             boolean isBirthDate = validateBirthDate(birthDateString);
 
             if (isBirthDate) {
-                LocalDate birthDate = LocalDate.parse(birthDateString);
-                LocalDate currentDate = LocalDate.now();
-                int age = currentDate.getYear() - birthDate.getYear();
-                if (age>0) {
-                    String dayOfWeek = birthDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault());
-                    int weekOfYear = birthDate.get(WeekFields.ISO.weekOfWeekBasedYear());
+                int age = calculateAge(birthDateString);
+                String dayOfWeek=calculateDayOfWeek(birthDateString);
+                int weekOfYear=calculateWeekOfYear(birthDateString);
+
+                if (age > 0) {
                     System.out.println("BirthDate " + birthDateString + " is valid");
                     System.out.println("Age " + age + " years");
                     System.out.println("Day of the week of birth " + dayOfWeek);
                     System.out.println("Week of the year of birth " + weekOfYear);
                     System.out.println("----------------------------------------");
-                }else {
+                } else {
                     System.out.println("BirthDate " + birthDateString + " is not valid");
                     System.out.println("----------------------------------------");
                 }
@@ -47,6 +46,24 @@ public class BirthDateValidator {
         Matcher matcher = pattern.matcher(birthDate);
         return matcher.matches();
     }
+
+    private static int calculateAge(String date) {
+        LocalDate birthDate = LocalDate.parse(date);
+        LocalDate currentDate = LocalDate.now();
+        int age = currentDate.getYear() - birthDate.getYear();
+        return age;
+    }
+
+    private static String calculateDayOfWeek(String date) {
+        LocalDate birthDate = LocalDate.parse(date);
+        String dayOfWeek = birthDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault());
+        return dayOfWeek;
+    }
+
+    private static int calculateWeekOfYear(String date) {
+        LocalDate birthDate = LocalDate.parse(date);
+        int weekOfYear = birthDate.get(WeekFields.ISO.weekOfWeekBasedYear());
+        return weekOfYear;
+    }
 }
-   
 
