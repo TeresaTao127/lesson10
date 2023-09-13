ca36bd376af9f879acb9972c146cb9562fb839e0
TeresaTao127 <taoleiluck2020@gmail.com>
2023-09-13 12:19:35 +0100

    lesson10 Task5 update unit test

--- src/main/java/lesson10Task5/PasswordValidator.java
+++ src/main/java/lesson10Task5/PasswordValidator.java
@@ -4,41 +4,31 @@ package lesson10Task5;
 import java.util.regex.*;
 
 public class PasswordValidator {
-    public static void main(String[] args) {
-        String[] passwords = {
-                "MyP@ssw1",
-                "javaFirstpro1",
-                "JAVAstudyIn2023",
-                "Pass1",
-                "23Password"
-        };
-
-
-        for (String password : passwords) {
-            boolean isPasswordValid = validatePassword(password);
-            if (isPasswordValid) {
-                System.out.println("Password '" + password + "' is valid");
-            } else {
-                System.out.println("Password '" + password + "' is not valid");
-            }
-        }
+  public static void main(String[] args) {
+    String password;
+    boolean isPasswordValid = validatePassword(password = "MyP@ssw1");
+    if (isPasswordValid) {
+      System.out.println("Password '" + password + "' is valid");
+    } else {
+      System.out.println("Password '" + password + "' is not valid");
     }
+  }
 
-    private static boolean validatePassword(String password) {
+  private static boolean validatePassword(String password) {
 
-        String uppercasePattern = ".*[A-Z].*";
+    String uppercasePattern = ".*[A-Z].*";
+    String digitPattern = ".*\\d.*";
+    String lengthPattern = ".{7,}";
+    String yearPattern = ".*(2023|23).*";
+    String passwordRegex = "^(?=.*" + uppercasePattern + ")(?=.*" + digitPattern + ")" +
+        "(?=" + lengthPattern + ")(?!" + yearPattern + ").*$";
 
-        String digitPattern = ".*\\d.*";
+    Pattern pattern = Pattern.compile(passwordRegex);
+    Matcher matcher = pattern.matcher(password);
+    return matcher.matches();
+  }
 
-        String lengthPattern = ".{7,}";
-
-        String yearPattern = ".*(2023|23).*";
-
-        String passwordRegex = "^(?=.*" + uppercasePattern + ")(?=.*" + digitPattern + ")" +
-                "(?=" + lengthPattern + ")(?!" + yearPattern + ").*$";
-
-        Pattern pattern = Pattern.compile(passwordRegex);
-        Matcher matcher = pattern.matcher(password);
-        return matcher.matches();
-    }
+  public static boolean isPasswordValid(String password) {
+    return validatePassword(password);
+  }
 }
